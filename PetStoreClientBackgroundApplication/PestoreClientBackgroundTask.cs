de24d92a7aee2399fd628b17a98bcc72ef8ed74d
vlastahajek <vlastimil.hajek@bonitoo.io>
2019-06-05 10:12:25 +0200

    Getting version from package info instead of hardcoded

--- PetStoreClientBackgroundApplication/PestoreClientBackgroundTask.cs
+++ PetStoreClientBackgroundApplication/PestoreClientBackgroundTask.cs
@@ -7,6 +7,7 @@ using Windows.ApplicationModel.Background;
 using PetStoreClientBackgroundApplication;
 using MetroLog;
 using MetroLog.Targets;
+using Windows.ApplicationModel;
 
 // The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
 
@@ -32,7 +33,7 @@ namespace PetStoreClientBackgroundApplication
                 // setup the global crash handler...
                 //GlobalCrashHandler.Configure();
                 Log = LogManagerFactory.DefaultLogManager.GetLogger<PestoreClientBackgroundTask>();
-                Log.Info("Started PestoreClientBackgroundTask 0.9.2");
+                Log.Info($"Started PestoreClientBackgroundTask {GetAppVersion()}");
                 man = WorkersManager.GetWorkersManager();
                 restRerver = new RestServer();
 #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
@@ -46,6 +47,15 @@ namespace PetStoreClientBackgroundApplication
                 Log.Fatal("Unexped exception", ex);
             }
             Log.Trace("Stopped");
+        }
+
+        private static string GetAppVersion()
+        {
+            Package package = Package.Current;
+            PackageId packageId = package.Id;
+            PackageVersion version = packageId.Version;
+
+            return string.Format("{0}.{1}.{2}", version.Major, version.Minor, version.Build);
         }
 
         private void TaskInstance_Canceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason)
